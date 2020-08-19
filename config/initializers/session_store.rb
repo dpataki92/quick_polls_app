94b4efeacc504dee8952966bf16578221c210931
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-08-19 22:13:18 +0200

    setting up cors and session configuration files

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+Rails.application.config.session_store :cookie_store, key: "_quick_polls_app", domain: "localhost:3000"
---
+++ db/schema.rb
@@ -0,0 +1,55 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_08_19_195255) do
+
+  create_table "options", force: :cascade do |t|
+    t.string "description"
+    t.integer "poll_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "polls", force: :cascade do |t|
+    t.string "question"
+    t.string "creator"
+    t.datetime "end_date"
+    t.integer "vote_requirement"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_friends", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+  end
+
+  create_table "user_polls", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "poll_id"
+  end
+
+  create_table "user_votes", force: :cascade do |t|
+    t.integer "option_id"
+    t.integer "user_id"
+    t.integer "poll_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
