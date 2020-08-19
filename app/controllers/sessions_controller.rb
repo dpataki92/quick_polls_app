3f46e5f1f150b881bb38681d9f91afff823599e7
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-08-19 22:44:37 +0200

    creating logged_in method and current_user_concern for setting current user before every action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+    include CurrentUserConcern
+
     def create
         user = User.find_by(username: params[:user][:username])
 
@@ -26,6 +28,19 @@ class SessionsController < ApplicationController
             else
                 render json: { message: "Sorry, data validation failed :(" }
             end
+        end
+    end
+
+    def logged_in
+        if @current_user
+            render json: {
+                logged_in: true,
+                user: @current_user
+            }
+        else 
+            render json: {
+                logged_in: false
+            }
         end
     end
 end
